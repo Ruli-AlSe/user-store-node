7a28e0ead65a2d63ca1bb8597e039b4618ced47e
Raul Almanza <ra.almanzaserrano@gmail.com>
2024-11-12 02:34:30 -0600

    Create a bcrypt adapter and encryp passwords before being saved in the DB

--- src/presentation/service/auth.service.ts
+++ src/presentation/service/auth.service.ts
@@ -1,3 +1,4 @@
+import { BcryptAdapter } from '../../config';
 import { UserModel } from '../../data';
 import { CustomError, RegisterUserDto, UserEntity } from '../../domain';
 
@@ -11,6 +12,7 @@ export class AuthService {
 
     try {
       const user = new UserModel(registerUserDto);
+      user.password = BcryptAdapter.hash(registerUserDto.password);
       await user.save();
 
       const { password, ...userEntity } = UserEntity.fromObject(user);
