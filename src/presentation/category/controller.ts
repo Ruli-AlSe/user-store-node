1654fdae0a5cc907f6da5cecdcb4d8cc05d5d2fd
Raul Almanza <ra.almanzaserrano@gmail.com>
2024-11-12 19:31:42 -0600

    Create a category service to communicate between model and controller when creating categories

--- src/presentation/category/controller.ts
+++ src/presentation/category/controller.ts
@@ -1,26 +1,30 @@
 import { Request, Response } from 'express';
 import { CreateCategoryDto, CustomError } from '../../domain';
+import { CategoryService } from '../service/category.service';
 
 export class CategoryController {
-  constructor() {}
+  constructor(private readonly categoryService: CategoryService) {}
 
-  private handleError(error: unknown, res: Response) {
+  private handleError = (error: unknown, res: Response) => {
     if (error instanceof CustomError) {
       return res.status(error.statusCode).json({ error: error.message });
     }
 
     console.log(` ${error}`);
     return res.status(500).json({ error: 'Internal server error' });
-  }
+  };
 
-  async createCategory(req: Request, res: Response) {
+  createCategory = (req: Request, res: Response) => {
     const [error, createCategoryDto] = CreateCategoryDto.create(req.body);
     if (error) return res.status(400).json({ error });
 
-    res.json({ createCategoryDto });
-  }
+    this.categoryService
+      .createCategory(createCategoryDto!, req.body.user)
+      .then((category) => res.status(201).json({ category }))
+      .catch((error) => this.handleError(error, res));
+  };
 
-  async getCategories(req: Request, res: Response) {
+  getCategories = (req: Request, res: Response) => {
     res.json('Get Categories');
-  }
+  };
 }
