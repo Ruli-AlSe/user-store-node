6e4fb63549a68de4041245eccd71a68ab81247ec
Raul Almanza <ra.almanzaserrano@gmail.com>
2024-11-13 19:13:51 -0600

    Create a middleware to handle image or images in the requests always as array

--- src/presentation/file-upload/routes.ts
+++ src/presentation/file-upload/routes.ts
@@ -2,12 +2,15 @@ import { Router } from 'express';
 
 import { UploadFileController } from './controller';
 import { FileUploadService } from '../service/file-upload.service';
+import { FileUploadMiddleware } from '../middlewares/file-upload.middleware';
 
 export class FileUploadRoutes {
   static get routes(): Router {
     const router = Router();
     const fileUploadService = new FileUploadService();
     const controller = new UploadFileController(fileUploadService);
+
+    router.use(FileUploadMiddleware.containFiles);
 
     router.post('/single/:type', controller.uploadFile);
     router.post('/multiple/:type', controller.uploadMultipleFiles);
